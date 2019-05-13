9b16248080896bf9b59c53d26e5ce5a59f31bd22
kerrydinh <kha.dinh@orientsoftware.net>
2019-05-13 13:37:14 +0700

    update

--- auth0-oidc-sample/src/App.js
+++ auth0-oidc-sample/src/App.js
@@ -1,11 +1,50 @@
-import React from 'react';
-import Routing from './Routing';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
+class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
 
-function App() {
-  return (
-    <Routing />
-  );
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  componentDidMount() {
+    const { renewSession } = this.props.auth;
+
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      renewSession();
+    }
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
+    return (
+      <div>
+        <Navbar bg="light" expand="lg" className={{ border: '1px solid #ccc' }}>
+          <Link className="nav-link" to="/">Home</Link>
+          <Link className="nav-link" to="/product">Product</Link>
+          <Link className="nav-link" to="/user">User</Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Form inline>
+              {!isAuthenticated() && (<Button onClick={this.login.bind(this)}>Login</Button>)}
+              {isAuthenticated() && (<Button onClick={this.logout.bind(this)}>Logout</Button>)}
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
--- auth0-oidc-sample/src/Routing.js
+++ auth0-oidc-sample/src/Routing.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link, Switch, Redirect } from "react-router-dom";
-import Home from './home/Home';
+import { Route, Router } from 'react-router-dom';
+import Product from './product/Product';
 import User from './user/User';
 import Callback from './callback/Callback';
-import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import Auth from './auth';
 import history from './history';
+import App from './App';
 
 const auth = new Auth();
 
@@ -17,59 +15,19 @@ const handleAuthentication = ({ location }) => {
     }
 }
 
-class Routing extends Component {
-    state = {}
-
-    onLogin = event => {
-        console.log('login');
-        const auth = new Auth();
-        auth.login();
-    };
-
-    onLogout() {
-        auth.logout();
-    }
-
-    componentDidMount() {
-        const { renewSession } = auth;
-
-        if (localStorage.getItem('isLoggedIn') === 'true') {
-            renewSession();
-        }
-    }
-
-    render() {
-        const isAuthenticated = auth.isAuthenticated();
-        console.log(isAuthenticated);
-        return (
-            <BrowserRouter history={history}>
-                <div>
-                    <Navbar bg="light" expand="lg" className={{ border: '1px solid #ccc' }}>
-                        <Link className="nav-link" to="/">Home</Link>
-                        <Link className="nav-link" to="/user">User</Link>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Form inline>
-                                { !auth.isAuthenticated() && (<Button onClick={this.onLogin}>Login</Button>)}
-                                { auth.isAuthenticated() && (<Button onClick={this.onLogout}>Logout</Button>)}
-                            </Form>
-                        </Navbar.Collapse>
-                    </Navbar>
-
-                    <Switch>
-                        <Route exact path="/" render={(props) => <Home auth={auth} {...props} />} />
-                        <Route exact path="/user" render={(props) => <User auth={auth} {...props} />} />
-                        <Route exact path="/callback" render={(props) => {
-                            console.log(props);
-                            handleAuthentication(props);
-                            return <Callback {...props} /> 
-                        }}/>
-                    </Switch>
-
-                </div>
-            </BrowserRouter>
-        );
-    }
-}
-
-export default Routing;
+export const makeMainRoutes = () => {
+    return (
+        <Router history={history}>
+          <div>
+            <Route path="/" render={(props) => <App auth={auth} {...props} />} />
+            <Route path="/product" render={(props) => <Product auth={auth} {...props} />} />
+            <Route path="/user" render={(props) => <User auth={auth} {...props} />} />
+            <Route path="/callback" render={(props) => {
+              handleAuthentication(props);
+              return <Callback {...props} /> 
+            }}/>
+          </div>
+        </Router>
+    );
+  }
+  
